e53d4d59d779d615aa1e595427e33f6e9fcdc1c9
Andrew Och <andrewoch95@gmail.com>
2018-07-10 23:04:09 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -2,13 +2,13 @@
 
 def dictionary
   dictionary_hash = {"hello" => "hi", 
-  "to" => '2',
+  "to" => "2",
   "two" => "2",
   "too" => "2",
-  "for" => '4',
-  "four" => '4',
-  'be' => 'b',
-  'you' => 'u',
+  "for" => "4",
+  "four" => "4",
+  "be" => "b",
+  "you" => "u",
   "at" => "@", 
   "and" => "&"}
   
