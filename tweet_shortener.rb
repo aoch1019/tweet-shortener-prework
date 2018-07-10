46970deffee884e8560e6dc733b7f6cd9e39fdb6
Andrew Och <andrewoch95@gmail.com>
2018-07-10 23:25:09 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -38,7 +38,9 @@ def bulk_tweet_shortener(array_of_tweets)
 end
 
 def selective_tweet_shortener(tweet)
-    if tweet.size > 140
+    if tweet =
+    
+    if tweet.length > 140
       puts word_substituter(tweet)
     else
       puts tweet
